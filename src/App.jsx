ab56d14809c51baf623e9015e72d1fde65fac3c2
4eJIoBeG <pavelshashenko@gmail.com>
2024-01-02 12:35:26 +0300

    add hero section and sliding text animations

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./app.scss";
+import Hero from "./components/Hero/Hero";
 import Navbar from "./components/Navbar/Navbar";
 
 const App = () => {
@@ -6,6 +7,7 @@ const App = () => {
     <div>
       <section id="Homepage">
         <Navbar />
+        <Hero />
       </section>
       <section id="Services">Parallax</section>
       <section>Services</section>
