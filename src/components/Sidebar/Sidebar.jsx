e9fd9990db4feca7a01d136c98a9ab0f89487e9f
4eJIoBeG <pavelshashenko@gmail.com>
2024-01-02 11:50:53 +0300

    add sidebar menu links animations

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -10,7 +10,7 @@ const Sidebar = () => {
   const variants = {
     open: {
       clipPath: "circle(1200px at 50px 50px)",
-      transition: { type: "spring", stiffness: 20 },
+      transition: { type: "spring", stiffness: 400, damping: 40 },
     },
     closed: {
       clipPath: "circle(30px at 50px 50px)",
