1eb42c36471a1a6d566b3aebf8d3e774060f12d8
4eJIoBeG <pavelshashenko@gmail.com>
2024-01-03 23:55:42 +0300

    fix sliding text in hero section

--- src/components/Services/Services.jsx
+++ src/components/Services/Services.jsx
@@ -17,7 +17,7 @@ const variants = {
 const Services = () => {
   const ref = useRef();
 
-  const isInView = useInView(ref, { margin: "100px" });
+  const isInView = useInView(ref, { margin: "-100px" });
 
   return (
     <motion.div
